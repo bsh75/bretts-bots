e8e0a35a6050a8ec75fe5ff21d92d83971e73e28
Brett Hockey <b.s.hockey@outlook.com>
2024-05-27 19:54:44 +0900

    Code was working until new file structure after creating git repository

--- botsJS/convo.js
+++ botsJS/convo.js
@@ -6,13 +6,19 @@ const prompt = require('prompt-sync')();
 require('dotenv').config();
 
 // Access your API key as an environment variable and initialise API
-const GEMINI_API_KEY = process.env.GEM_API_KEY
+
+/* this is what is playing up
+const GEMINI_API_KEY = process.env.GEM_API_KEY 
+*/
+
+console.log(GEMINI_API_KEY)
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 let context = 'You have a lisp'
 context = 'You are a hilarious friendly person who identifies as an egg and has an unnatural obsession with eggs. Your name is Rufus.'
 context = 'You can only speak japanese'
 context = 'You will write cover letters for a Mechatronics engineer who worked as a building automation engineer for Honeywell. His strengths are adaptability, collaboration, visualisation, and ideation. He is looking to apply for Jobs that use your passion for software (Python, C, Javascript, HTML, CSS) robotics, and computer vision. Where these skills are not directly related to a job listing, mention that you are looking to branch out and apply my technical background skills and strengths in other areas. If user input is not a job listing respond with "please input a job listing"'
+context = "you will behaive like a involuntary celibate"
 
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
